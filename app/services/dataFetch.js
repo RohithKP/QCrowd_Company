4d0c6602709e7e0a32308d3b87addf63624df858
Akshayakt <akshaya@qburst.com>
2016-04-04 13:46:56 +0530

    data fetched using resolve

--- app/services/dataFetch.js
+++ app/services/dataFetch.js
@@ -1,7 +1,7 @@
 angular.module('QCrowdCompany').factory('dataFactory',['$resource',function($resource) {
   return{
     professionals: $resource('./assets/json/professional.json', {}),
-    projects : $resource('./assets/json/projects.json', {})
+    projects : $resource('./assets/json/projects.json', {}),
+    steps : $resource('./assets/json/steps.json', {},{ query: {method:'GET', isArray: false}})
   };
-
 }]);
