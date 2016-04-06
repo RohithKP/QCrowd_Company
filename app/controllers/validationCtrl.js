c858dd5cee5e71ca1d23f1e21dee62a59589fd9c
Akshayakt <akshaya@qburst.com>
2016-04-06 12:45:12 +0530

    solved validation issue

--- app/controllers/validationCtrl.js
+++ app/controllers/validationCtrl.js
@@ -3,20 +3,13 @@ angular.module('QCrowdCompany').controller('validationCtrl',['$scope','$state',f
   $scope.login = function() {
     $scope.$broadcast('show-errors-check-validity');
     if ($scope.loginForm.$valid) {
-      $scope.reset();
        $state.go('dashboard');
     }
   };
   $scope.submit = function () {
     $scope.$broadcast('show-errors-check-validity');
     if ($scope.registerForm.$valid) {
-      $scope.reset();
       $state.go('login');
     }
-  }
-  $scope.reset = function() {
-    $scope.$broadcast('show-errors-reset');
-    $scope.register ={ name: '', email: '', website : '', contact: '', number: '' };
-    $scope.user = { name: '', password: ''};
-  }
+  };
 }]);
