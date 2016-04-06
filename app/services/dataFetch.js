e1c34a2f32cf42c3493a92071a3dd0f0960b224e
Akshayakt <akshaya@qburst.com>
2016-04-06 12:49:16 +0530

    changed datafetch dervice

--- app/services/dataFetch.js
+++ app/services/dataFetch.js
@@ -2,6 +2,6 @@ angular.module('QCrowdCompany').factory('dataFactory',['$resource',function($res
   return{
     professionals: $resource('./assets/json/professional.json', {}),
     projects : $resource('./assets/json/projects.json', {}),
-    steps : $resource('./assets/json/steps.json/:id', {id:"@id"},{ query: {method:'GET', isArray: false}})
+    steps : $resource('./assets/json/steps.json',{},{ query: {method:'GET', isArray: false}})
   };
 }]);
